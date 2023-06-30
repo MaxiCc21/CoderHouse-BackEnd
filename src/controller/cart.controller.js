cabdebfc38716f05ce787b7506cff8ceac36de22
MaxiCc21 <87211484+MaxiCc21@users.noreply.github.com>
2023-06-30 02:57:21 -0300

    refactor: :recycle: MVC cart

---
+++ src/controller/cart.controller.js
@@ -0,0 +1,24 @@
+const { cartService } = require("../service/idex");
+
+class cartController {
+  cartGET = async (req, res) => {
+    console.log("/api/carts");
+    const jwtUser = req.user;
+
+    const productDataUser = await cartService.getItemToCart(jwtUser.sub);
+    if (!productDataUser.ok) {
+      res.status(400).send("Algo salio mal al cargar los productos");
+    }
+    console.log(productDataUser.statusMsj);
+    const options = {
+      title: "Carrito de compras",
+      style: "cart.css",
+      products: productDataUser.data,
+      usercookie: jwtUser,
+    };
+
+    res.render("cart/cart.handlebars", options);
+  };
+}
+
+module.exports = new cartController();
