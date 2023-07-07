ad4b7c3d2cecacb3fcb4f00681d64bd562a04fb9
MaxiCc21 <87211484+MaxiCc21@users.noreply.github.com>
2023-07-07 17:12:43 -0300

    refactor: :art: Main.js / Edit Home.html

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,12 +1,12 @@
 /* Función para mostrar/ocultar el menú desplegable */
 function myFunction() {
-  d.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
 /* Cierra el menú desplegable si el usuario hace clic fuera de él */
 window.onclick = function (event) {
   if (!event.target.matches(".dropbtn")) {
-    var dropdowns = d.getElementsByClassName("dropdown-content");
+    var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
