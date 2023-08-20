cc45958bda67f7a98640ecf629f634b5a8962e9f
MaxiCc21 <87211484+MaxiCc21@users.noreply.github.com>
2023-08-20 20:05:57 -0300

    .

--- src/routes/prueba.routes.js
+++ src/routes/prueba.routes.js
@@ -12,6 +12,32 @@ router.use(
   })
 );
 
+router.get("/simple", (req, res) => {
+  let suma = 0;
+
+  for (let i = 0; i < 10000000; i++) {
+    suma += 1;
+  }
+  // artillery quick --count 40 --num 50   'http://localhost:8080/prueba/simple' -o simple.json
+  res.send({
+    status: "succes",
+    mesage: `El worker ${procces.id} a atendido esta peticion, resultado es ${suma}`,
+  });
+});
+
+router.get("/complejo", (req, res) => {
+  let suma = 0;
+
+  for (let i = 0; i < 5e8; i++) {
+    suma += 1;
+  }
+
+  res.send({
+    status: "succes",
+    mesage: `El worker ${procces.id} a atendido esta peticion, resultado es ${suma}`,
+  });
+});
+
 router.get("/stringmuylargo", async (req, res) => {
   let string = "Hola coders, soy un string ridiculamente largo";
   for (let i = 0; i < 5e4; i++) {
