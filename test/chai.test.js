665eb844b32ead047a541c41883ab625426a13b0
MaxiCc21 <87211484+MaxiCc21@users.noreply.github.com>
2023-09-10 17:34:29 -0300

    .

---
+++ test/chai.test.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const UserDao = require("../src/dao/MongoManager/UserManager");
+const Assert = require("assert");
+require("dotenv").config();
+const chai = require("chai");
+
+mongoose.connect(process.env.MONGO_URL_DB);
+
+const expect = chai.expect;
+
+describe("Set de test User en chai", () => {
+  before(function () {
+    this.userDao = new UserDao();
+  });
+  beforeEach(function () {
+    // mongoose.connect.collections.MercadoLibre.users.drop();
+    this.timeout(5000);
+  });
+  it("el dao debe poder obtener todos los usuarioen un arreglo", async () => {
+    this.timeout(5000);
+    const result = await this.userDao.getAllUser();
+    console.log(result);
+    expect(result).to.be.deep.equal([]);
+  });
+});
