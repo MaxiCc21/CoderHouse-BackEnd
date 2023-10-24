dad73a2667b4b9405100cb9174f41fec644e0dae
MaxiCc21 <87211484+MaxiCc21@users.noreply.github.com>
2023-10-24 02:55:47 -0300

    alert()

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,23 +1,25 @@
-const d = document;
-
-let elementos = d.querySelectorAll("#text");
+let elementos = document.querySelectorAll("#text");
 
 elementos.forEach(function (span) {
   if (span.textContent.length > 68) {
     span.style.bottom = -15;
   }
 });
-if (logoutButton) {
-  logoutButton.addEventListener("click", (e) => {
-    fetch("/home", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  });
-}
+// if (logoutButton) {
+//   logoutButton.addEventListener("click", (e) => {
+//     fetch("/home", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
+//   });
+// }
 
+if (paymentStatusMessage) {
+  alert(paymentStatusMessage);
+  window.location.href = "/home";
+}
 const $productTitleP = document.querySelectorAll("#productTitle");
 
 $productTitleP.forEach(function (p) {
